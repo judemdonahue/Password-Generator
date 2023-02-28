f34727baaa925e2b6bbc4a9a4b12d5e07a3c96db
AntiqueRised <imjoodie@gmail.com>
2023-02-27 21:54:59 -0600

    Set variables for password input options, defined generatePassword function, set chars to apply using ternary operator.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,38 @@
-// Assignment code here
+// HTML input variables
+var passwordText = document.querySelector("#password");
+var lowerEl = document.querySelector("#input1");
+var upperEl = document.querySelector("#input2");
+var numberEl = document.querySelector("#input3");
+var specEl = document.querySelector("#input4");
+var lengthEl = document.querySelector("#lengthInput");
 
-
-// Get references to the #generate element
+// Char Strings
 var generateBtn = document.querySelector("#generate");
+var charsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charsLower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var specialChars = "!@#$%^&*()";
+
+// Setting generatePassword function
+function generatePassword() {
+  let pass = "";
+  let length = lengthEl.value;
+  console.log(length)
+  let chars = "";
+
+  // Applying Char strings based on what boxes are checked using ternary operator
+  chars += lowerEl.checked ? charsLower : "";
+  chars += upperEl.checked ? charsUpper : "";
+  chars += numberEl.checked ? numbers : "";
+  chars += specEl.checked ? specialChars : "";
+
+  for (var i = 0; i <= length; i++) {
+    let rand = Math.floor(Math.random() * chars.length);
+    pass += chars.substring(rand, rand +1);
+  }
+
+  return pass;
+}
 
 // Write password to the #password input
 function writePassword() {
